48841fa6563e9d82665a8e3830b10d9ef46f864a
Siwei Wu <62462693+Wusiwei0410@users.noreply.github.com>
2023-06-14 09:26:45 +0800

    Add files via upload

---
+++ CPNC-S/Contrastive_Pretraining_ConceptNet.py
@@ -0,0 +1,233 @@
+
+
+"""
+The system trains BERT (or any other transformer model like RoBERTa, DistilBERT etc.) on the SNLI + MultiNLI (AllNLI) dataset
+with MultipleNegativesRankingLoss. Entailnments are poisitive pairs and the contradiction on AllNLI dataset is added as a hard negative.
+At every 10% training steps, the model is evaluated on the STS benchmark dataset
+Usage:
+python training_nli_v2.py
+OR
+python training_nli_v2.py pretrained_transformer_model_name
+"""
+import math
+from sentence_transformers import models, losses, datasets
+from sentence_transformers import LoggingHandler, SentenceTransformer, util, InputExample
+from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
+import logging
+from datetime import datetime
+import sys
+import os
+import gzip
+import csv
+import random
+
+datasets_name = 'ConceptNet'
+#datasets_name = 'Atomic'
+neg_num = 1
+repeated_neg_num = 1
+
+#### Just some code to print debug information to stdout
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO,
+                    handlers=[LoggingHandler()])
+#### /print debug information to stdout
+
+#model_name = sys.argv[1] if len(sys.argv) > 1 else 'distilroberta-base'
+model_name = 'bert-large-uncased'
+train_batch_size = 128          #The larger you select this, the better the results (usually). But it requires more GPU memory
+max_seq_length = 50
+num_epochs = 1
+
+# Save path of the model
+model_save_path = f'./CP_model/{datasets_name}'
+
+
+# Here we define our SentenceTransformer model
+print(model_name)
+word_embedding_model = models.Transformer(model_name, max_seq_length=max_seq_length)
+pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension(), pooling_mode='mean')
+model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
+
+
+# Read the AllNLI.tsv.gz file and create the training dataset
+logging.info("Read AllNLI train dataset")
+
+print('read_train_data')
+def add_to_samples(sent1, sent2, label):
+    if sent1 not in train_data:
+        train_data[sent1] = {'contradiction': set(), 'entailment': set()}
+    train_data[sent1][label].add(sent2)
+
+
+nodes = []
+head_nodes = {}
+if datasets_name == 'ConceptNet':
+    f = open('./data/ConceptNet/train.txt', 'r', encoding = 'utf-8')
+elif datasets_name == 'Atomic':
+    f = open('./data/atomic/train.preprocessed.txt', 'r', encoding = 'utf-8')
+train_txt = f.readlines()
+for line in train_txt:
+    line = line.replace('\n', '')
+    line = line.split('\t')[:3]
+    if datasets_name == 'ConceptNet':
+            head = line[1]
+    elif datasets_name == 'Atomic':
+            head = line[0]
+    tail = line[2]
+    
+    nodes.append(head)
+    nodes.append(tail)
+    
+    if head not in head_nodes:
+        head_nodes[head] = []
+    
+    head_nodes[head].append(tail)
+
+
+#remove repeated nodes
+for head in head_nodes:
+    head_nodes[head] = set(head_nodes[head])
+
+nodes = list(set(nodes))
+
+
+train_data = {}
+
+print('neg')
+
+if datasets_name == 'ConceptNet':
+    file_name = './data/ConceptNet/train.txt'
+    val_file_name = './data/ConceptNet/valid.txt'
+    test_file_name = './data/ConceptNet/test.txt'
+elif datasets_name == 'Atomic':
+    file_name = './data/atomic/train.preprocessed.txt'
+    val_file_name = './data/atomic/valid.preprocessed.txt'
+    test_file_name = './data/atomic/test.preprocessed.txt'
+ 
+with open(file_name, 'r', encoding = 'utf-8') as f:
+    train_txt = f.readlines()
+    count = 0
+    for line in train_txt:
+        #print(count)
+        count +=1
+        line = line.replace('\n', '')
+        line = line.split('\t')[:3]
+        
+        if datasets_name == 'ConceptNet':
+            head = line[1]
+        elif datasets_name == 'Atomic':
+            head = line[0]
+        tail = line[2]
+        add_to_samples(head, tail, 'entailment')
+        add_to_samples(tail, head, 'entailment')
+        
+        negs = random.choices(nodes, k = neg_num)
+        for i, neg in enumerate(negs):
+            if neg in head_nodes[head]:
+                while 1:
+                    neg = random.choice(nodes)
+                    if neg not in set(head_nodes[head]):
+                        break
+            negs[i] = neg
+            
+        for neg in negs:
+            add_to_samples(head, neg, 'contradiction')
+            add_to_samples(neg, head, 'contradiction')
+        
+        
+        #add_to_samples(sent2, sent1, row['label'])  #Also add the opposite
+
+
+train_samples = []
+for sent1, others in train_data.items():
+    if len(others['entailment']) > 0 and len(others['contradiction']) > 0:
+        tails = list(others['entailment'])
+        for i in range(len(tails)):
+            for j in range(repeated_neg_num):
+                train_samples.append(InputExample(texts=[sent1, tails[i], random.choice(list(others['contradiction']))]))
+                train_samples.append(InputExample(texts=[tails[i], sent1, random.choice(list(others['contradiction']))]))
+
+
+logging.info("Train samples: {}".format(len(train_samples)))
+
+
+print('Special data loader that avoid duplicates within a batch')
+# Special data loader that avoid duplicates within a batch
+train_dataloader = datasets.NoDuplicatesDataLoader(train_samples, batch_size=train_batch_size)
+
+
+print('Our training loss')
+# Our training loss
+train_loss = losses.MultipleNegativesRankingLoss(model)
+
+
+
+print('read_test_data')
+
+#Read STSbenchmark dataset and use it as development set
+logging.info("Read STSbenchmark dev dataset")
+dev_samples = []
+with open(val_file_name, 'r', encoding = 'utf-8') as f:
+    train_txt = f.readlines()
+    for line in train_txt:
+        line = line.replace('\n', '')
+        line = line.split('\t')[:3]
+        
+        if datasets_name == 'Atomic':
+            head = line[0]
+        elif datasets_name == 'ConceptNet':
+            head = line[1]
+        tail = line[2]
+        
+        dev_samples.append(InputExample(texts=[head, tail], label=float(1)))
+        dev_samples.append(InputExample(texts=[tail, head], label=float(1)))
+        neg = random.choice(nodes)
+        dev_samples.append(InputExample(texts=[head, neg], label=float(0)))
+        neg = random.choice(nodes)
+        dev_samples.append(InputExample(texts=[tail, neg], label=float(0)))
+
+dev_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(dev_samples, batch_size=train_batch_size, name='sts-dev')
+
+# Configure the training
+warmup_steps = math.ceil(len(train_dataloader) * num_epochs * 0.1) #10% of train data for warm-up
+logging.info("Warmup-steps: {}".format(warmup_steps))
+
+print('start training')
+# Train the model
+model.fit(train_objectives=[(train_dataloader, train_loss)],
+          evaluator=dev_evaluator,
+          epochs=num_epochs,
+          evaluation_steps=int(len(train_dataloader)*0.05),
+          warmup_steps=warmup_steps,
+          output_path=model_save_path,
+          use_amp=False          #Set to True, if your GPU supports FP16 operations
+          )
+
+
+
+##############################################################################
+#
+# Load the stored model and evaluate its performance on STS benchmark dataset
+#
+##############################################################################
+test_samples = []
+with open(test_file_name, 'r', encoding = 'utf-8') as f:
+    train_txt = f.readlines()
+    for line in train_txt:
+        line = line.replace('\n', '')
+        line = line.split('\t')[:3]
+        
+        if datasets_name == 'Atomic':
+            head = line[0]
+        elif datasets_name == 'ConceptNet':
+            head = line[1]
+        tail = line[2]
+        
+        test_samples.append(InputExample(texts=[head, tail], label=float(1)))
+        neg = random.choice(nodes)
+        test_samples.append(InputExample(texts=[head, neg], label=float(0)))
+
+model = SentenceTransformer(model_save_path)
+test_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(test_samples, batch_size=train_batch_size, name='sts-test')
+test_evaluator(model, output_path=model_save_path)
--- README.md
+++ README.md
@@ -1,1 +1,117 @@
-# CPNC
+# Commonsense Knowledge Graph Completion Via Contrastive Pretraining and Node Clustering
+
+
+Code for the paper [Commonsense Knowledge Graph Completion Via Contrastive Pretraining and Node Clustering](https://arxiv.org/pdf/2305.17019.pdf). 
+
+## Bibtex
+
+```
+@article{DBLP:journals/corr/abs-2305-17019,
+  author       = {Siwei Wu and
+                  Xiangqing Shen and
+                  Rui Xia},
+  title        = {Commonsense Knowledge Graph Completion Via Contrastive Pretraining
+                  and Node Clustering},
+  journal      = {CoRR},
+  volume       = {abs/2305.17019},
+  year         = {2023},
+  url          = {https://doi.org/10.48550/arXiv.2305.17019},
+  doi          = {10.48550/arXiv.2305.17019},
+  eprinttype    = {arXiv},
+  eprint       = {2305.17019},
+  timestamp    = {Wed, 07 Jun 2023 14:31:13 +0200},
+  biburl       = {https://dblp.org/rec/journals/corr/abs-2305-17019.bib},
+  bibsource    = {dblp computer science bibliography, https://dblp.org}
+}
+```
+
+## Requirements
+
+- PyTorch
+- Run `pip install -r requirements.txt` to install the required packages.
+
+
+
+## Training 
+
+### Data
+
+The dataset, semantic embedding of node and nodes clustering data are provided in [CPNC-S-data](https://pan.baidu.com/s/1AyUGZlUT2-ri2Sj5w_6Yzg?pwd=om9e ) and [CPNC-I-data ](https://pan.baidu.com/s/1DyMJs_lumfAf5GsPELsEEg?pwd=xe4j ). Plese donwload it and unzip the file under CPNC-S and CPNC-I, respectively. Because we didn't set the random seed in Constrastive Pretraining and Nodes Clustering, so we advice you use the embedding we have obtained.
+
+### Semantic Embedding and Node Clustering
+
+ You can rerun the result as following:
+
+```bash
+cd CPNC/CPNC-S
+```
+
+Train Constrastive Pretraining Model for ATOMIC and CN-100K datasets:
+
+```bash
+python Contrastive_Pretraining_ATOMIC.py
+python Contrastive_Pretraining_ConceptNet.py
+```
+
+This trains the model and saves the model under the `/CP_model/ATOMIC/`  and `/CP_model/ConceptNet/` directory. Based on it, you can get the semantic embedding of node :
+
+```bash
+python get_nodes_embedding_Atomic.py
+python get_nodes_embedding_ConceptNet.py
+```
+
+the semantic embedding of node are saved in `/bert_model_embeddings/nodes-lm-atomic/`  and `/bert_model_embeddings/nodes-lm-conceptnet/` directory.
+
+Then, clustering nodes to obtain the latent concept:
+
+```bash
+python K_means_Atomic.py
+python K_means_ConceptNet.py
+```
+
+The nodes cluster result are saved in `/Concept_Centre/atomic/`  and `/Concept_Centre/ConceptNet/`.
+
+### CPNC-S Model
+
+To train the CPNC-S model ,  you need to run the following command:
+
+```bash
+cd CPNC/CPNC-S
+```
+
+Finally, in order to train the CPNC-S model on CN-100K, run the following command:
+
+```bash
+python -u src/run_kbc_subgraph.py --dataset conceptnet --evaluate-every 10 --n-layers 2 --graph-batch-size 60000  --bert_concat --Concept_center_path './Concept_Centre/ConceptNet/'
+```
+
+In order to train the CPNC-S model on ATOMIC, run the following command:
+
+```bash
+python -u src/run_kbc_subgraph.py --dataset atomic  --evaluate-every 10 --n-layers 2 --graph-batch-size 20000  --bert_concat --Concept_center_path './Concept_Centre/atomic/'
+```
+
+This trains the model and saves the model under the`./saved_models_ConceptNet/` and  `./saved_models_ATOMIC/` directory.
+
+### CPNC-I Model
+
+To train the CPNC-I model ,  you need to run the following command:
+
+```bash
+cd CPNC/CPNC-I
+```
+
+Then, in order to train the CPNC-I model on CN-100K, run the following command:
+
+```bash
+bash train.sh conceptnet-100k 15 saved/saved_ckg_model data/saved_entity_embedding/conceptnet/cn_bert_emb_dict.pkl 500 256 100 ConvTransE 10 1234 1e-20 0.25 0.25 0.25 0.0003 1024 Adam 5 300 RWGCN_NET 50000 1324 data/saved_entity_embedding/conceptnet/cn_fasttext_dict.pkl 300 0.2 5 100 50 0.1 ./Concept_Centre/ConceptNet/
+```
+
+In order to train the CPNC-S model on ATOMIC, run the following command:
+
+```bash
+bash train.sh atomic 500 saved/saved_ckg_model data/saved_entity_embedding/atomic/at_bert_emb_dict.pkl 500 256 100 ConvTransE 10 1234 1e-20 0.20 0.20 0.20 0.0001 1024 Adam 5 300 RWGCN_NET 50000 1324 data/saved_entity_embedding/atomic/at_fasttext_dict.pkl 300 0.2 3 100 50 0.1 ./Concept_Centre/atomic/python -u src/run_kbc_subgraph.py --dataset atomic  --evaluate-every 10 --n-layers 2 --graph-batch-size 20000  --bert_concat --Concept_center_path './Concept_Centre/atomic/'
+```
+
+This trains the model and saves the model under the `saved/saved_ckg_model/` directory.
+
